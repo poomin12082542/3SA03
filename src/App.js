838a3af255c7a1c48a5df150a7d051c7163e9f8d
Your Namepoomin12082542 <poomin12082542@hotmail.com>
2020-09-15 16:07:05 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import CharacterCard from './CharacterCard';
+const word = "Hello";
+function App() {
+ return (
+ <div>
+ {
+ Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
+ }
+ </div>
+ )
+}
+export default App;
